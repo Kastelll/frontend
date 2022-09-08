c25d5b4404b077bb951f667c9ef333b7d4e47f51
TheTeaCup <lw0482352@gmail.com>
2022-09-07 22:24:29 -0400

    working on a temp public page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import {ChakraProvider} from '@chakra-ui/react'
 import theme from "../utils/theme";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <title>Kastel - Coming Soon</title>
+                <meta property="og:title" content={'Kastel - Coming Soon'} key="title"/>
+                <meta property="og:url" content={'https://statuspage.theteacup.dev'}/>
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     )
