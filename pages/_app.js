d414ef49241379831e374c0577e7774be7fb9af6
TheTeaCup <lw0482352@gmail.com>
2022-09-26 19:40:07 -0400

    Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import {ChakraProvider} from '@chakra-ui/react'
 import theme from "../utils/theme";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({Component, pageProps}) {
     return (
         <ChakraProvider theme={theme}>
+            <Script async defer data-website-id="d9fda726-51ca-4d2a-85d8-60afdb5e1790" src="https://analytics.kastelapp.com/umami.js"></Script>
             <Head>
                 <title>Kastel - Coming Soon</title>
                 <meta property="og:title" content={'Kastel - Coming Soon'} key="title"/>
